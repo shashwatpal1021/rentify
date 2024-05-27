54a8747b507f463ca8f21825d5d09e42d4a19221
shashwatpal1021 <shashwatpal1021@gmail.com>
2024-05-27 08:06:47 +0530

    Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,13 +3,14 @@ import React, { useEffect, useState } from 'react'
 import PropertyCard from '../components/PropertyCard';
 import { usePropertyState } from '../atoms';
 import axios from 'axios';
+import Hero from './Hero';
+import Properties from '../components/Properties';
 
 const Home = () => {
   const [properties, setProperties] = usePropertyState()
   const [loading, setLoading] = useState(true)
 
   // const navigate = useNavigate();
-
 
   useEffect(() => {
     axios.get("http://localhost:3000/api/properties").then(res => {
@@ -19,19 +20,11 @@ const Home = () => {
     }).catch(err => console.log(err))
   }, [])
 
-
-
+  // console.log("properties", properties)
   return (
     <>
-      {loading ? <Typography>Loading</Typography> : (
-        <Grid container spacing={2} p={2}>
-          {properties.map((data) => (
-            <Grid item xs={12} md={4}>
-              <PropertyCard data={data} />
-            </Grid>
-          ))}
-
-        </Grid> )}
+      <Hero/>
+      <Properties />
     </>
   )
 }
